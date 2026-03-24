ca378026855ccdb18f4d7a54475e15b83b8e88a9
taewony <engchat@gmail.com>
2026-03-24 13:26:16 +0900

    env setup

--- learn_by_examples/javascript/js/script.js
+++ learn_by_examples/javascript/js/script.js
@@ -33,3 +33,23 @@ colorSelect.addEventListener("change", function () {
   let target = document.body;
   target.style.backgroundColor = this.value; // colorSelect.value
 });
+
+var links = document.querySelectorAll("a");
+links.forEach(function (link) {
+  link.addEventListener("focus", function (event) {
+    event.preventDefault();
+    this.style.color = "red";
+    this.style.fontWeight = "bold";
+    this.style.backgroundColor = "#ccc";
+  });
+});
+
+window.addEventListener("keydown", function (event) {
+  let userInput = event.key;
+  console.log("사용자가 입력한 키:", userInput);
+  if (userInput === "ArrowLeft") {
+    this.alert("왼쪽 화살표 키가 눌렸습니다.");
+  } else if (userInput === "ArrowRight") {
+    this.alert("오른쪽 화살표 키가 눌렸습니다.");
+  }
+});
